989c1502ac4728098f9ab11f88a8c049950ce298
Artem Smirnov <parov91@mail.ru>
2023-01-15 10:17:54 +0300

    added header

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,6 @@
 $(function () {
-
+  $('.header__menu-btn').on('click', function () {
+    $('.header__list').toggleClass('header__list--active');
+    $('.header__menu-btn').toggleClass('header__menu-btn--active');
+  })
 })
